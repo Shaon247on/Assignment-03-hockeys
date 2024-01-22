82b9ca352289f059f2189fb980ee72a8eb2ebdf3
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-01-22 20:31:58 +0600

    added the navigation bar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+            'font-manrope': ['Manrope', 'sans-serif']            
+        }
+      },
+
+    },
+    plugins: [],
+
+  }
